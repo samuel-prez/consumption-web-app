9fe1348d2bbc5d2b1940e090772be9f1f7f3997a
samuel-prez <zetrax190@gmail.com>
2024-08-02 15:39:36 -0500

    prime install

---
+++ consumption-app/src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      main-menu works!
+    </p>
+  `,
+  styles: ``
+})
+export class MainMenuComponent {
+
+}
