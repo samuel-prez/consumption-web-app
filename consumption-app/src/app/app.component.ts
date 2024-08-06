113a9d3f664ce98889c0f09f0035fc81e9c7fa11
samuel-prez <zetrax190@gmail.com>
2024-08-06 07:42:04 -0500

    ventana principal v1.0

--- consumption-app/src/app/app.component.ts
+++ consumption-app/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent,UserComponent],
+  imports: [RouterOutlet, UserComponent, UserComponent, MainMenuComponent,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'consumption-app';
 }
+
+
--- consumption-app/src/app/main-menu/main-menu.component.ts
+++ consumption-app/src/app/main-menu/main-menu.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import {StyleClassModule} from 'primeng/styleclass';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      main-menu works!
-    </p>
-  `,
-  styles: ``
+  imports: [ButtonModule],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
 })
 export class MainMenuComponent {
 
